b5807ff9855a14f014ca0f49b0fd0ba0ff70b359
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-30 10:41:16 +0100

    added css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from "./logo.svg"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./css/Roberto.css"
+import "./css/Hilal.css"
+import "./css/Evgeni.css"
 import "./App.css"
 import { Container, Row, Col } from "react-bootstrap"
 
