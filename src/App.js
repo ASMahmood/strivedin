bdab3dadce5da02622b3cfa5675ec5fb55f137b3
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-30 18:10:30 +0100

    lastest fixes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./css/Roberto.css"
 import "./css/Hilal.css"
 import "./css/Evgeni.css"
 import "./App.css"
-import Sidebar from './components/Sidebar'
-import Sidebar0 from './components/Sidebar0'
+import Sidebar from "./components/Sidebar"
+import Sidebar0 from "./components/Sidebar0"
 import { Container, Row, Col } from "react-bootstrap"
 import OurNavBar from "./components/OurNavBar"
 import GenericBody from "./components/GenericBody"
@@ -15,23 +15,23 @@ import MyJumbotron from "./components/MyJumbotron"
 function App() {
 	return (
 		<>
-    <OurNavBar />
+			<OurNavBar />
 			<Container fluid>
-          <Row>
-					<Col md={9}>{/**here goes the body*/}
-            	<MyJumbotron />
-						  <GenericBody />
-          </Col>
+				<Row>
+					<Col md={9} className="bodyColumn">
+						{/**here goes the body*/}
+						<MyJumbotron />
+						<GenericBody />
+					</Col>
 					<Col md={3}>
-						<Sidebar0/>
+						<Sidebar0 />
 						<Sidebar title="People also viewed" />
 						<Sidebar title="People you may know" />
-            <GenericSidebar />
-
+						<GenericSidebar />
 					</Col>
 				</Row>
 				<Row>{/**here goes the footer*/}</Row>
-			</Container >
+			</Container>
 			{/**bonus if we get to the chat that should stick to the bottom of the viewport*/}
 		</>
 	)
