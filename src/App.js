3e19c464915ba68741508c8d64975af92969f2e0
Evgeni67 <evgeni313@abv.bg>
2020-11-30 14:23:41 -0500

    jumbotron

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./css/Roberto.css"
-import "./css/Hilal.css"
-import "./css/Evgeni.css"
-import "./App.css"
-import { Container, Row, Col } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/Roberto.css";
+import "./css/Hilal.css";
+import "./css/Evgeni.css";
+import "./App.css";
+import MyJumbotron from "./components/MyJumbotron";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
-	return (
-		<>
-			<Container>
-				<Row>{/*here goes the navbar*/}</Row>
-				<Row>
-					<Col md={10}>{/**here goes the body*/}</Col>
-					<Col md={2}>{/**here goes the sidebar */}</Col>
-				</Row>
-				<Row>{/**here goes the footer*/}</Row>
-			</Container>
-			{/**bonus if we get to the chat that should stick to the bottom of the viewport*/}
-		</>
-	)
+  return (
+    <>
+      <Container>
+        <Row>{/*here goes the navbar*/}</Row>
+        <Row>
+          <Col md={10}>
+            <MyJumbotron />
+          </Col>
+          <Col md={2}>{/**here goes the sidebar */}</Col>
+        </Row>
+        <Row>{/**here goes the footer*/}</Row>
+      </Container>
+      {/**bonus if we get to the chat that should stick to the bottom of the viewport*/}
+    </>
+  );
 }
 
-export default App
+export default App;
