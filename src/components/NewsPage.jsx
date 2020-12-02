676bef51f8374f33af7066b028cec64ab88f99e4
Evgeni67 <evgeni313@abv.bg>
2020-12-02 15:56:46 -0500

    Left SideBar added
    :alien:

--- src/components/NewsPage.jsx
+++ src/components/NewsPage.jsx
@@ -1,51 +1,55 @@
-import React from "react"
-import Sidebar from "./Sidebar"
-import Sidebar0 from "./Sidebar0"
-import { Container, Row, Col } from "react-bootstrap"
-
-import GenericBody from "./GenericBody"
-import Body from "./Body.jsx"
-import GenericSidebar from "./GenericSidebar"
-import MyJumbotron from "./MyJumbotron"
-import Footer from "./Footer"
-import { withRouter } from "react-router-dom"
+import React from "react";
+import Sidebar from "./Sidebar";
+import Sidebar0 from "./Sidebar0";
+import { Container, Row, Col } from "react-bootstrap";
+import LeftSidebarNewsPage from "./leftSideBarNewsPage";
+import GenericBody from "./GenericBody";
+import Body from "./Body.jsx";
+import GenericSidebar from "./GenericSidebar";
+import MyJumbotron from "./MyJumbotron";
+import Footer from "./Footer";
+import { withRouter } from "react-router-dom";
 
 /**
  * we will neeed this when we start routing stuff
  */
 class Newsfeed extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	gotId = () => {
-		return this.props.match.params.id ? this.props.match.params.id : "me"
-	}
+  gotId = () => {
+    return this.props.match.params.id ? this.props.match.params.id : "me";
+  };
 
-	render() {
-		return (
-			<>
-				<Container>
-					<Row>
-						<Col className="leftSidebar"></Col>
-						<Col md={9} className="bodyColumn"></Col>
-						<Col md={3}>
-							<Sidebar0 />
-							<Sidebar title="Our Team" />
-							<Sidebar title="People also viewed" />
-							<Sidebar title="People you may know" />
-							<GenericSidebar />
-						</Col>
-					</Row>
-					<Row>
-						<Footer />
-					</Row>
-				</Container>
-				{/**bonus if we get to the chat that should stick to the bottom of the viewport*/}
-			</>
-		)
-	}
+  render() {
+    return (
+      <>
+        <Container>
+          <Row>
+            <Col md={3} className="leftSidebar">
+              <LeftSidebarNewsPage />
+            </Col>
+            <Col md={6} className="bodyColumn">
+              {" "}
+            </Col>
+            <Col md={3}>
+              <Sidebar0 />
+              <Sidebar title="Our Team" />
+              <Sidebar title="People also viewed" />
+              <Sidebar title="People you may know" />
+              <GenericSidebar />
+            </Col>
+          </Row>
+          <Row>
+            <Footer />
+          </Row>
+        </Container>
+        {/**bonus if we get to the chat that should stick to the bottom of the viewport*/}
+      </>
+    );
+  }
 }
 
-export default withRouter(Newsfeed)
+export default withRouter(Newsfeed);
