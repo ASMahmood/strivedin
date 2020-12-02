c3d8592ae0252ab4c7bd61837d3126186a86477f
Evgeni67 <evgeni313@abv.bg>
2020-12-02 10:51:53 -0500

    Respoinsivness
    Resoinsivness of Jumbotron and YourDashboard :heavy_check_mark:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from "react"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css"
-import "./css/Roberto.css"
-import "./css/Hilal.css"
-import "./css/Evgeni.css"
-import "./App.css"
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import Profile from "./components/Profile.jsx"
-
-import OurNavBar from "./components/OurNavBar"
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/Roberto.css";
+import "./css/Hilal.css";
+import "./css/Evgeni.css";
+import "./App.css";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Profile from "./components/Profile";
 
 function App() {
-	console.log("env in app.js", process.env)
-	return (
-		<>
-			<OurNavBar />
-			<Router>
-				<Route exact path="/" component={Home} />
-				<Route path="/profile/:id" component={Profile} />
-			</Router>
-		</>
-	)
+  return (
+    <>
+      <Router>
+        <Route exact path="/" component={Profile} />
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
